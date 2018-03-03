42a498c479a435bb788c9ecab6c539039f08ffe2
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-03-03 15:16:11 +0200

    Wrap constants in a singleton CONSTANTS class

---
+++ CONSTANTS.java
@@ -0,0 +1,8 @@
+public class CONSTANTS {
+    private CONSTANTS() {
+    }
+
+    public final static int SINGLE_ENGINE = 1;
+    public final static int TURBO = 2;
+    public final static int JET = 3;
+}
