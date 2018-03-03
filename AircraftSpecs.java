4e9ac4e0bd5fccb75e79c554d43e24e5be9009df
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-03-03 13:36:06 +0200

    Implement AircraftSpecs class for storing technical infos for each aircraft type

---
+++ AircraftSpecs.java
@@ -0,0 +1,52 @@
+public class AircraftSpecs {
+    private int type;
+    private String name;
+    private int landingSpeed, takeOffSpeed; // Landing and Takeoff speed in knots
+    private int maxFlightSpeed; // Normal flight speed in knots
+    private int maxAlt; // Maximum Altitude in feets
+    private int ascDescRate; // Ascent and Descent Rate in ft/min
+    private int fuelConsumptionRate; //
+    private int maxFuelWeight;
+
+    public AircraftSpecs(int type, String name, int landingSpeed, int maxFlightSpeed,
+        int maxFuelWeight, int maxAlt, int ascDescRate, int fuelConsumptionRate) {
+
+        this.type = type;
+        this.name = name;
+        this.landingSpeed = landingSpeed;
+        this.takeOffSpeed = landingSpeed;
+        this.maxFlightSpeed = maxFlightSpeed;
+        this.maxAlt = maxAlt;
+        this.ascDescRate = ascDescRate ;
+        this.fuelConsumptionRate = fuelConsumptionRate;
+        this.maxFuelWeight = maxFuelWeight;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getLandingSpeed() {
+        return landingSpeed;
+    }
+
+    public int getTakeOffSpeed() {
+        return takeOffSpeed;
+    }
+
+    public int getMaxAlt() {
+        return maxAlt;
+    }
+
+    public int getAscDescRate() {
+        return ascDescRate;
+    }
+
+    public int getFuelConsumptionRate() {
+        return fuelConsumptionRate;
+    }
+
+    public int getMaxFuelWight() {
+        return maxFuelWeight;
+    }
+}
