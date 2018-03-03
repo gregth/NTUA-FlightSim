74e99026eb0b8202482ce13bc18d0fd58266445d
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-03-03 11:05:00 +0200

    Implement Position class

---
+++ Position.java
@@ -0,0 +1,25 @@
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void print() {
+        System.out.println("x: " + x + " y: " + y);
+    }
+
+    public String stringify() {
+        return this.x + " " + this.y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
