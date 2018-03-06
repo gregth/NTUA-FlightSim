556d8250ba155fd64ef9245f9ab641a334980fdc
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-03-06 16:21:27 +0200

    Implent real and simulation time indicator in status pane

--- StatusPane.java
+++ StatusPane.java
@@ -2,20 +2,23 @@ import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import java.util.*;
 
 public class StatusPane extends JPanel {
     private static final int STATUS_HEIGHT = CONSTANTS.STATUS_HEIGHT;
     private static final int STATUS_WIDTH = CONSTANTS.STATUS_WIDTH;
+    private JLabel simulatedTimeLabel, aircraftsLabel, crashesLabel, landingsLabel, realTimeLabel;
 
-    private JLabel simulatedTimeLabel, aircraftsLabel, crashesLabel, landingsLabel;
-	private int timeElapsedInSec, aircrafts, crashes, landings;
-
-	public StatusPane (){
+	public StatusPane () {
+        super();
 		setLayout(new GridBagLayout());
         this.setPreferredSize(new Dimension(STATUS_WIDTH, STATUS_HEIGHT));
 
 		simulatedTimeLabel = new JLabel("Simulated Time: 00:00", JLabel.LEFT);
 		this.add(simulatedTimeLabel);
+
+		realTimeLabel = new JLabel("Real Time: 00:00", JLabel.LEFT);
+		this.add(realTimeLabel);
 
 		aircraftsLabel = new JLabel("Aircrafts: 0", JLabel.LEFT);
         this.add(aircraftsLabel);
@@ -26,45 +29,26 @@ public class StatusPane extends JPanel {
 		landingsLabel = new JLabel("Landings: 0", JLabel.LEFT);
         this.add(landingsLabel);
 
-        timeElapsedInSec = 0;
-        aircrafts = 0;
-        crashes = 0;
-        landings = 0;
-
 		setBackground(Color.CYAN);
+        this.repaint();
 	}
 
-	public int getTimeElapsed() {
-		return timeElapsedInSec;
-	}
-
-	public void increaseAircrcafts() {
-		aircrafts++;
-		aircraftsLabel.setText("Aircrafts: " + aircrafts);
-	}
-
-	public void increaseCrashes() {
-		crashes++;
-		crashesLabel.setText("Crashes: " + crashes);
-	}
-
-	public void increaseLandings() {
-		landings++;
-		landingsLabel.setText("Landings: " + landings);
-	}
-
-    public void increaseTime(int intervalInSeconds) {
-        timeElapsedInSec += intervalInSeconds;
-        displayTime(timeElapsedInSec);
-    }
-
-	public void displayTime(int timeInSeconds) {
-        int minutes = timeElapsedInSec / 60;
-        int seconds = timeElapsedInSec % 60;
+	public String displayTime(int timeInSeconds) {
+        System.out.println("TIME IN SECONDS:" + timeInSeconds);
+        int minutes = timeInSeconds / 60;
+        int seconds = timeInSeconds % 60;
 
         String minutesString = String.format("%02d", minutes);
         String secondsString = String.format("%02d", seconds);
+		return new String(minutesString + ":" + secondsString);
+	}
 
-		simulatedTimeLabel.setText("Simulated Time: " + minutesString + ":" + secondsString);
+	public void refresh() {
+        Universe myUniverse = Universe.getInstance();
+		landingsLabel.setText("Landings: " + myUniverse.getLandings());
+		aircraftsLabel.setText("Aircrafts: " + myUniverse.getAircrafts());
+		crashesLabel.setText("Crashes: " + myUniverse.getCrashes());
+		simulatedTimeLabel.setText("Simulated Time: " + displayTime(myUniverse.getSimulatorClock()));
+		realTimeLabel.setText("Real Time: " + displayTime((int)(CONSTANTS.TIME_RATIO * myUniverse.getSimulatorClock())));
 	}
 }
