a68513af3c893f199550c9a5afd623fe3fc83c68
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-03-06 16:31:12 +0200

    Implement airports and aircrafts painting

--- MapPane.java
+++ MapPane.java
@@ -1,10 +1,11 @@
-import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.util.*;
+import java.awt.geom.AffineTransform;
 
 public class MapPane extends JPanel {
     private static final int MAP_WIDTH = CONSTANTS.MAP_WIDTH;
@@ -17,14 +18,12 @@ public class MapPane extends JPanel {
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setColor(Color.BLACK);
-		g2d.fillOval(0, 0, 30, 30);
-		g2d.drawOval(0, 50, 30, 30);
-		g2d.fillRect(50, 0, 30, 30);
-		g2d.drawRect(50, 50, 30, 30);
         paintMapTiles(g2d);
+        paintAircrafts(g2d);
 	}
 
 	public MapPane (){
+        super();
         this.setLayout(new GridLayout());
         this.setPreferredSize(new Dimension(MAP_WIDTH, MAP_HEIGHT));
         this.setMinimumSize(new Dimension(MAP_WIDTH, MAP_HEIGHT));
@@ -33,7 +32,7 @@ public class MapPane extends JPanel {
         this.repaint();
 	}
 
-    public void paintMapTiles(Graphics2D g2d) {
+    private void paintMapTiles(Graphics2D g2d) {
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
                 paintTile(g2d, i, j);
@@ -41,21 +40,67 @@ public class MapPane extends JPanel {
         }
     }
 
+    private void paintAircrafts(Graphics2D g2d) {
+        ArrayList<Flight> flights = FlightsDatabase.getInstance().getActiveFlights();
+        for (Flight flight : flights) {
+            paintAircraft(g2d, flight);
+        }
+    }
+
     // First column is number 0, first row is number 0
-    public void paintTile(Graphics2D g2d, int tileRow, int tileColumn) {
+    private void paintTile(Graphics2D g2d, int tileRow, int tileColumn) {
         Color color = Map.getInstance().getColor(tileRow, tileColumn);
 		g2d.setColor(color);
-        int startX = TILE_SIZE * tileColumn;
-        int startY = TILE_SIZE * tileRow;
+        int startX = tileColumn * TILE_SIZE;
+        int startY = tileRow * TILE_SIZE;
 		g2d.fillRect(startX, startY, TILE_SIZE , TILE_SIZE);
+		g2d.setColor(Color.BLACK);
+		g2d.drawRect(startX, startY, TILE_SIZE , TILE_SIZE);
 
         // Draw airport pin, if it exists
-        if (AirportDatabase.getInstance().existsAirport(tileRow, tileColumn)) {
+        Airport airport = AirportDatabase.getInstance().findAirport(tileRow, tileColumn);
+        if (airport != null) {
             Image image = new ImageIcon("icons/airport.png").getImage();
-            int iconX = startX + TILE_SIZE/2 - 1;
-            int iconY = startY + TILE_SIZE/2 - 1;
-            System.out.println(iconX + " " + iconY);
+            int iconX = milesToPixels(airport.getPosition()).getX() - image.getWidth(null) / 2 ;
+            int iconY = milesToPixels(airport.getPosition()).getY() - image.getHeight(null) / 2;
             g2d.drawImage(image, iconX, iconY, this);
         }
+    }
+
+    private void paintAircraft(Graphics2D g2d, Flight flight) {
+        String name = "small_n.png";
+        System.out.println("DEGREES :" + flight.getDegrees());
+        switch (flight.getDegrees()) {
+            case Flight.UP:
+                name = "small_n.png";
+                break;
+            case Flight.DOWN:
+                name = "small_s.png";
+                break;
+            case Flight.RIGHT:
+                name = "small_e.png";
+                break;
+            case Flight.LEFT:
+                name = "small_w.png";
+        }
+        Image image = new ImageIcon("icons/" + name).getImage();
+        PrecisePosition p = flight.getAircraftPosition();
+        p.print();
+        int iconX = milesToPixels(p).getX()- image.getWidth(null) / 2 ;
+        int iconY = milesToPixels(p).getY()- image.getHeight(null) / 2 ;
+        System.out.println("Airxraft location in pixels: " + iconX + " " + iconY);
+        g2d.drawImage(image, iconX, iconY, this);
+    }
+
+    private Position milesToPixels(Position p) {
+        int x = (int) (((double)p.getX()) * CONSTANTS.PIXEL_TO_MILES_RATIO);
+        int y = (int) (((double)p.getY()) * CONSTANTS.PIXEL_TO_MILES_RATIO);
+        return new Position(x, y);
+    }
+
+    private Position milesToPixels(PrecisePosition p) {
+        int x = (int) (p.getX() * CONSTANTS.PIXEL_TO_MILES_RATIO);
+        int y = (int) (p.getY() * CONSTANTS.PIXEL_TO_MILES_RATIO);
+        return new Position(x, y);
     }
 }
